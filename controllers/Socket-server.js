5d198400484652472400539ea24e3d93377f3c38
moshOntong <moshontong@gmail.com>
2022-05-29 13:48:28 +0800

    stable for now

--- controllers/Socket-server.js
+++ controllers/Socket-server.js
@@ -4,6 +4,48 @@ import { SERVER_DB_URL } from "../config/variables";
 import axios from "axios";
 const router = express.Router();
 
+router.put("/update/ticket/:id", async (req, res) => {
+  const { id } = req.params;
+  await axios
+    .put(`${SERVER_DB_URL}/api/update/ticket/${id}`, {
+      headers: {
+        // Overwrite Axios's automatically set Content-Type
+        "Content-Type": "application/json",
+      },
+    })
+    .then(async (_) => {
+      const { data } = await axios.get(SERVER_DB_URL + "/api/tickets");
+      io.emit("update-ticket", data);
+      res.send(data);
+    });
+});
+
+router.post("/add/ticket/comment", async (req, res) => {
+  const { ticketId, createdBy } = req.query;
+  await axios
+    .post(
+      `${SERVER_DB_URL}/api/add/ticket/comment?ticketId=${ticketId}&createdBy=${createdBy}`,
+      req.body,
+      {
+        headers: {
+          // Overwrite Axios's automatically set Content-Type
+          "Content-Type": "application/json",
+        },
+      }
+    )
+    .then(async (_) => {
+      const { data } = await axios.get(
+        SERVER_DB_URL + `/api/tickets/comments/${ticketId}`
+      );
+      io.emit("add-comment", data);
+    })
+    .then(async (_) => {
+      const { data } = await axios.get(SERVER_DB_URL + "/api/tickets");
+      io.emit("update-ticket", data);
+      res.send(data);
+    });
+});
+
 router.post("/add/ticket", async (req, res) => {
   const { createdBy } = req.query;
   // console.log(req.query);
